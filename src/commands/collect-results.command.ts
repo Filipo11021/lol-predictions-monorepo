426ad884790860583c0110301bc029b7699912ec
Filipo <filipfilip280@gmail.com>
2023-10-23 01:44:17 +0200

    fix: defer response of collect command

--- src/commands/collect-results.command.ts
+++ src/commands/collect-results.command.ts
@@ -24,105 +24,106 @@ export const data = new SlashCommandBuilder()
 export const execute = async (
   interaction: ChatInputCommandInteraction<CacheType>
 ) => {
-  try {
-    const isPublic = interaction.options.getBoolean("public");
-    const gameDayId = interaction.options.getString("id");
+  const isPublic = interaction.options.getBoolean("public");
+  const gameDayId = interaction.options.getString("id");
 
-    const data = !!gameDayId
-      ? await db.gameDay.findUnique({
-          where: { id: gameDayId },
-          include: {
-            games: {
-              include: {
-                voters: {
-                  include: {
-                    user: {
-                      include: {
-                        _count: {
-                          select: {
-                            votes: true,
-                          },
+  await interaction.deferReply({ephemeral: !isPublic});
+
+  const dataPromise = !!gameDayId
+    ? await db.gameDay.findUnique({
+        where: { id: gameDayId },
+        include: {
+          games: {
+            include: {
+              voters: {
+                include: {
+                  user: {
+                    include: {
+                      _count: {
+                        select: {
+                          votes: true,
                         },
                       },
                     },
-                    team: true,
+                  },
+                  team: true,
+                },
+              },
+              teams: true,
+            },
+          },
+        },
+      })
+    : (
+         db.currentGameDay.findUnique({
+          where: { id: "main" },
+          include: {
+            gameDay: {
+              include: {
+                games: {
+                  include: {
+                    voters: {
+                      include: {
+                        user: {
+                          include: {
+                            _count: {
+                              select: {
+                                votes: true,
+                              },
+                            },
+                          },
+                        },
+                        team: true,
+                      },
+                    },
+                    teams: true,
                   },
                 },
-                teams: true,
               },
             },
           },
         })
-      : (
-          await db.currentGameDay.findUnique({
-            where: { id: "main" },
-            include: {
-              gameDay: {
-                include: {
-                  games: {
-                    include: {
-                      voters: {
-                        include: {
-                          user: {
-                            include: {
-                              _count: {
-                                select: {
-                                  votes: true,
-                                },
-                              },
-                            },
-                          },
-                          team: true,
-                        },
-                      },
-                      teams: true,
-                    },
-                  },
-                },
-              },
-            },
-          })
-        )?.gameDay;
+      )
 
-    const allCount = await db.game.count();
+  const [data, allCount] = await Promise.all([dataPromise, db.game.count()])
 
-    const result = data?.games.map(({ voters, teams, id, type }) => ({
-      all_votes_count: allCount,
-      teams: teams.map(({ code, name, image }) => ({ code, name, image })),
-      type: type ?? $Enums.MatchType.BO3,
-      id,
-      voters: voters.map(({ team, user: { username, id, _count }, score }) => ({
-        username,
-        teamCode: team.code,
-        teamName: team.name,
-        user_id: id,
-        score,
-        votes_count: _count.votes,
-      })),
-    }));
-
-    if (!result) {
-      await interaction.reply({
-        content: "Brak danych",
-        ephemeral: isPublic === true ? false : true,
-      });
-      return;
-    }
-
-    const date = new Date();
-    const fileName = `results_${date.getHours()}-${date.getMinutes()}_${date.getDate()}.${date.getMonth()}.${date.getFullYear()}.json`;
-
-    fs.writeFileSync(fileName, JSON.stringify(result), { encoding: "utf8" });
-
-    await interaction.reply({
-      files: [fileName],
-      content: "results:",
-      ephemeral: isPublic === true ? false : true,
+  if (!data) {
+    await interaction.editReply({
+      content: "Brak danych",
     });
-
-    fs.unlinkSync(fileName);
-  } catch (error) {
-    console.log(error);
-    throw Error();
+    return;
   }
+
+  const result = ("gameDay" in data ? data.gameDay : data)?.games.map(({ voters, teams, id, type }) => ({
+    all_votes_count: allCount,
+    teams: teams.map(({ code, name, image }) => ({ code, name, image })),
+    type: type ?? $Enums.MatchType.BO3,
+    id,
+    voters: voters.map(({ team, user: { username, id, _count }, score }) => ({
+      username,
+      teamCode: team.code,
+      teamName: team.name,
+      user_id: id,
+      score,
+      votes_count: _count.votes,
+    })),
+  }));
+
+  if (!result) {
+    await interaction.editReply({
+      content: "Brak danych",
+    });
+    return;
+  }
+
+  const date = new Date();
+  const fileName = `results_${date.getHours()}-${date.getMinutes()}_${date.getDate()}.${date.getMonth()}.${date.getFullYear()}.json`;
+
+  fs.writeFileSync(fileName, JSON.stringify(result), { encoding: "utf8" });
+
+  await interaction.editReply({
+    files: [fileName],
+  });
+
+  fs.unlinkSync(fileName);
 };
