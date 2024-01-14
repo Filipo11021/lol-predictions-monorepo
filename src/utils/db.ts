d632ecff2879bb504183f82efac027291a6dd20c
Filipo <filipfilip280@gmail.com>
2024-01-14 23:17:59 +0100

    fix: create prisma singleton for dev mode

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var db: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+export const db = globalThis.db ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.db = db
