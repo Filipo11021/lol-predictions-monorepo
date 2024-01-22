444de704898f2023c11a5400273b9de9ef8442e7
Filipo <filipfilip280@gmail.com>
2024-01-22 22:00:44 +0100

    fix: types and schema incompatibility to backend response

--- src/schema/events.schema.ts
+++ src/schema/events.schema.ts
@@ -1,30 +1,31 @@
-import { z } from "zod";
+import { ZodSchema, ZodTypeAny, ZodUnion, z } from "zod";
+
+const mainEventSchema = z.object({
+  startTime: z.string(),
+  state: z.string(),
+  type: z.string(),
+  blockName: z.string(),
+  league: z.object({ name: z.string(), slug: z.string() }),
+  match: z.object({
+    id: z.string(),
+    teams: z.array(
+      z.object({
+        name: z.string(),
+        code: z.string(),
+        image: z.string(),
+        result: z.object({
+          outcome: z.null().or(z.string()),
+          gameWins: z.number(),
+        }),
+        record: z.object({ wins: z.number(), losses: z.number() }),
+      })
+    ),
+    strategy: z.object({ type: z.string(), count: z.number() }),
+  }),
+});
 
 const eventSchema = z.union([
-  z.object({
-    startTime: z.string(),
-    state: z.string(),
-    type: z.string(),
-    blockName: z.string(),
-    league: z.object({ name: z.string(), slug: z.string() }),
-    match: z.object({
-      id: z.string(),
-      flags: z.array(z.string()),
-      teams: z.array(
-        z.object({
-          name: z.string(),
-          code: z.string(),
-          image: z.string(),
-          result: z.object({
-            outcome: z.string(),
-            gameWins: z.number(),
-          }),
-          record: z.object({ wins: z.number(), losses: z.number() }),
-        })
-      ),
-      strategy: z.object({ type: z.string(), count: z.number() }),
-    }),
-  }),
+  mainEventSchema,
   z.object({
     startTime: z.string(),
     state: z.string(),
@@ -35,35 +36,18 @@ const eventSchema = z.union([
       image: z.string(),
     }),
   }),
-  z.object({
-    startTime: z.string(),
-    state: z.string(),
-    type: z.string(),
-    blockName: z.string(),
-    league: z.object({ name: z.string(), slug: z.string() }),
-    match: z.object({
-      id: z.string(),
-      flags: z.array(z.unknown()),
-      teams: z.array(
-        z.object({
-          name: z.string(),
-          code: z.string(),
-          image: z.string(),
-          result: z.object({ outcome: z.null(), gameWins: z.number() }),
-          record: z.object({ wins: z.number(), losses: z.number() }),
-        })
-      ),
-      strategy: z.object({ type: z.string(), count: z.number() }),
-    }),
-  }),
 ]);
 
 export const scheduleResponseSchema = z.object({
   data: z.object({
     schedule: z.object({
-      events: z.array(eventSchema),
+      events: z
+        .array(eventSchema)
+        .transform((arr) =>
+          arr.filter((data) => mainEventSchema.safeParse(data).success)
+        ) as unknown as ZodSchema<z.infer<typeof mainEventSchema>[]>,
     }),
   }),
 });
 
-export type EventT = z.infer<typeof eventSchema>;
+export type EventT = z.infer<typeof mainEventSchema>;
