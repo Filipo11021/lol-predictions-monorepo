35e9c494db24724b40188587fe44b8bdabefce84
Filipo <filipfilip280@gmail.com>
2024-01-28 22:47:27 +0100

    fix(frontend): handling of prisma in monorepo

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,13 @@
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			config.plugins = [...config.plugins, new PrismaPlugin()];
+		}
+
+		return config;
+	},
+};
 
 module.exports = nextConfig;
