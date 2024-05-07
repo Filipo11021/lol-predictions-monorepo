9997626dc60cbf6f5fd2496f9bb959b7f0e4b119
Filipo <filipfilip280@gmail.com>
2024-05-07 10:59:30 +0200

    feat: add clear version of votes widget

---
+++ apps/frontend/src/app/widget/[id]/clear/page.tsx
@@ -0,0 +1,33 @@
+import { prisma } from '@repo/database';
+
+export default async function WidgetPage({
+	params,
+}: { params: { id: string } }) {
+	const game = await prisma.game.findUnique({
+		where: {
+			id: params.id,
+		},
+	});
+	const votes = await Promise.all(
+		game?.teamCodes.map(async (teamCode) => ({
+      count: await prisma.vote.count({ where: { gameId: params.id, teamCode: teamCode } }),
+      teamCode
+    })
+		) ?? []
+	);
+
+
+
+	return <div className="p-3 rounded-sm text-white flex max-w-fit gap-4 items-center">
+    <ScoreCard count={votes[0].count} teamCode={votes[0].teamCode} />
+    <div>VS</div>
+    <ScoreCard count={votes[1].count} teamCode={votes[1].teamCode} />
+  </div>;
+}
+
+function ScoreCard ({count, teamCode}: {teamCode: string, count: number}) {
+  return <div className='flex justify-center items-center flex-col'>
+    <div className='text-xl font-medium'>{teamCode}</div>
+    <div className='font-extrabold text-2xl'>{count}</div>
+  </div>
+}
